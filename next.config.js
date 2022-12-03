7166aece43fe6edd200acc0eecf289ae5222f828
s92i <sofiane.92izii@gmail.com>
2022-12-04 00:46:46 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  distDir: 'build',
+  images: {
+    domains: ['yt3.ggpht.com', 's01.pic4net.com', 'lh3.googleusercontent.com'],
+  }
 }
 
 module.exports = nextConfig
